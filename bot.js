66037c4614f089e45e71c4fe0e8d1789a5335e65
brucehan15 <77654243+brucehan15@users.noreply.github.com>
2021-01-22 09:41:39 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity('the world burn', { type: 'WATCHING' });
+    client.user.setActivity('best girl make bread', { type: 'WATCHING' });
 });
 
 
